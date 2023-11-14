f17bb8e7f0029e16bef34179de57d383f9f7dc99
Reza-Riswandha <emrezariswanda@gmail.com>
2023-11-14 16:48:50 +0700

    Add text data pelanggan on HalamanSatu

--- app/src/main/java/com/example/activity5/ui/page/HalamanSatu.kt
+++ app/src/main/java/com/example/activity5/ui/page/HalamanSatu.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.activity5.R
@@ -41,6 +43,10 @@ fun HalamanSatu(
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxWidth()
     ){
+        Row {
+            Text(text = "Data Pelanggan", fontWeight = FontWeight.Bold)
+        }
+        Spacer(modifier = Modifier.height(10.dp))
 
         OutlinedTextField(
             value = namaText,
